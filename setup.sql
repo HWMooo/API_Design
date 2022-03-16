d01fca62a5802a964c2cb82ee53acc56552bedad
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-03-16 10:55:11 +0000

    schema and tables created

--- setup.sql
+++ setup.sql
@@ -1,27 +1,33 @@
-DROP TABLE IF EXISTS People
-CREATE TABLE People {
-    ownerId PRIMARY KEY int,
+DROP TABLE IF EXISTS People;
+CREATE TABLE People (
+    ownerId INT,
     name VARCHAR,
+    Age INT,
+    numberOfPeople INT
+);
+COPY People
+FROM $str$/code/data/People.csv$str$ 
+DELIMITER ',' CSV HEADER;
 
+DROP TABLE IF EXISTS Houses;
+CREATE TABLE Houses (
+    AddressId INT,
+    ownerId INT
 
-
-}
-
-DROP TABLE IF EXISTS Houses
-CREATE TABLE Houses {
-    AddressId PRIMARY KEY int,
-    ownerId int
     
-}
+);
+COPY Houses
+FROM $str$/code/data/Houses.csv$str$ 
+DELIMITER ',' CSV HEADER;
 
 
-DROP TABLE IF EXISTS Addresses
-CREATE TABLE Addresses {
-    AddressId PRIMARY KEY int,
+DROP TABLE IF EXISTS Addresses;
+CREATE TABLE Addresses (
+    AddressId INT,
     Address VARCHAR,
     PostCode VARCHAR
-    
-
-
-}
+);
+COPY Addresses
+FROM $str$/code/data/Addresses.csv$str$ 
+DELIMITER ',' CSV HEADER;
 
